0179e469cb7aa900f3b3249464257582a12684c7
fannyjian <fannyj@u.nus.edu>
2023-09-18 11:46:24 +0800

    connect backend to frontend

--- user-service/backend/index.js
+++ user-service/backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import mysql from "mysql2"
+import cors from "cors"
 
 const app = express();
 
@@ -12,6 +13,7 @@ const db = mysql.createConnection({
 
 // allow users to send requests via json
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res)=>{
     res.json("hi backend")
