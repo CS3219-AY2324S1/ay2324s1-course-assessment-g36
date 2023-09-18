aa63e6a69a83839ee81f08fe92f14fc2e74e9066
fannyjian <fannyj@u.nus.edu>
2023-09-18 11:54:48 +0800

    enable add user from frontend

--- user-service/frontend/src/App.js
+++ user-service/frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Users from './pages/Users';
+import Add from './pages/Add';
+import { Update } from './pages/Update';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- user-service/frontend/src/pages/Add.jsx
+++ user-service/frontend/src/pages/Add.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Add = () => {
   const[user, setUser]=useState({
     email:"",
     password:""
-  })
+  });
+
+  const navigate = useNavigate()
+
+  const handleChange= (e) => {
+    setUser(prev=>({...prev, [e.target.name]: e.target.value}));
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8000/users", user);
+      navigate("/");
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className='form'>
       <h1>Add new user</h1>
-      <input type="text" placeholder='email'/>
-      <input type="text" placeholder='password'/>
+      <input type="text" placeholder='email' onChange={handleChange} name="email"/>
+      <input type="text" placeholder='password' onChange={handleChange} name="password"/>
+      <button onClick={handleClick}>Add</button>
     </div>
   )
 }
