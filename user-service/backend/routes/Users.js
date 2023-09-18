6d1934745ddeaec28afca9f04e9b6ddfe8d4b995
fannyjian <fannyj@u.nus.edu>
2023-09-18 16:41:47 +0800

    allow password hashing and user validation

--- user-service/backend/routes/Users.js
+++ user-service/backend/routes/Users.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const { Users } = require('../models');
+const bcrypt = require("bcrypt");
 
+router.post("/register", async (req, res) => {
+    const {Username, Email, Password } = req.body;  
+    bcrypt.hash(Password, 10).then((hash) => {
+        Users.create({
+            Username: Username,
+            Email: Email,
+            Password: hash,
+        })
+        res.json("SUCCESS")
+    })
+})
+
+router.post("/login", async (req, res) => {
+    const { Email, Password } = req.body;
+    const user = await Users.findOne({ where: { Email: Email } });
+
+    if (!user) {
+        res.json({ error: "User doesn't exist" });
+    } else {
+        bcrypt.compare(Password, user.Password).then((match) => {
+            if (!match) {
+                res.json({ error: "Incorrect password!" });
+            } else {
+                res.json("yay logged in");
+            }
+        });
+    }
+});
 
 router.get("/", async (req, res) => {
     const userList = await Users.findAll();
@@ -11,12 +40,6 @@ router.get("/", async (req, res) => {
 router.get("/:UserId", async (req, res)=> {
     const id = req.params.UserId
     const user = await Users.findByPk(id);
-    res.json(user);
-})
-
-router.post("/", async (req, res) => {
-    const user = req.body;  
-    await Users.create(user);
     res.json(user);
 })
 
