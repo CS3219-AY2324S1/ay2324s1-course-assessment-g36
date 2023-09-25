7bed476656365b3f2303ff812fb110a3221e3742
Sebastian Lee <leehur_sebastian@hotmail.com>
2023-09-25 21:37:22 +0800

    [SL] Show error message on profile list page

--- frontend/src/components/Users/ProfileList/ProfileList.tsx
+++ frontend/src/components/Users/ProfileList/ProfileList.tsx
@@ -2,38 +2,49 @@ import { useState, useEffect } from "react"
 import { fetchAllUsers } from "@/utils/userApi"
 import { User } from "@/interfaces";
 import ProfileCard from "../ProfileCard/ProfileCard";
-import { Stack, SimpleGrid, Heading } from "@chakra-ui/react";
+import { Stack, SimpleGrid, Heading, Text } from "@chakra-ui/react";
 import SkeletonLoader from "@/components/Loader/SkeletonLoader";
+
+enum Status {
+  Error,
+  Loading,
+  Success
+}
 
 export default function ProfileList(): JSX.Element {
 
   const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [status, setStatus] = useState<Status>(Status.Loading);
 
   async function fetchData() {
     try {
       const results = await fetchAllUsers();
       setUsers(results);
+      setStatus(Status.Success)
     } catch (error) {
-      console.log("Error fetching users;")
+      setStatus(Status.Error)
     }
   }
 
   useEffect(() => {
     fetchData();
-    setIsLoading(false)
   }, []);
 
+  if (status == Status.Loading) {
+    return <SkeletonLoader />
+  }
+
+  if (status == Status.Error) {
+    return <Text color='red'>Failed to load user profiles. Please try again later.</Text>
+  }
+
   return (
-    <>
-      {isLoading
-        ? <SkeletonLoader />
-        : <Stack spacing='16px'>
-          <Heading as='h2' size='lg' textAlign='center'>User Profiles</Heading>
-          <SimpleGrid spacing={6} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
-            {users.map(user => <ProfileCard key={user.userId} user={user} />)}
-          </SimpleGrid>
-        </Stack>}
-    </>
+      <Stack spacing='16px'>
+        <Heading as='h2' size='lg' textAlign='center'>User Profiles</Heading>
+        <SimpleGrid spacing={6} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
+          {users.map(user => <ProfileCard key={user.userId} user={user} />)}
+        </SimpleGrid>
+      </Stack>
   )
+
 }
