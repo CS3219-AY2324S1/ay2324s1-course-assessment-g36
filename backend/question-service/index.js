3bf2cba95f54d42abd2c9ec00fc037b84725862c
fannyjian <fannyj@u.nus.edu>
2023-10-26 18:05:34 +0800

    install kafka and add docker files

--- backend/question-service/index.js
+++ backend/question-service/index.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const cors = require("cors");
 const connectToDb = require("./config/db")
+const kafka = require('kafka-node')
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// const client = new kafka.KafkaClient({kafkahost: process.env.KAFKA_BOOTSTRAP_SERVERS})
+// const producer = new kafka.Producer(client)
+// producer.on('ready', () => {
+//     // app.post('/', async (req, res) => {
+//     //     producer.send([{topic: process.env.KAFKA_TOPIC,
+//     //         messages: JSON.stringify(req, body)}], async (err, data) => {
+//     //             if (err) console.log(err)
+//     //             else res.send("hi")
+//     //         }
+//     //     )
+//     // })
+//     console.log("producer ready")
+// })
 
 connectToDb();
 
--- backend/user-service/index.js
+++ backend/user-service/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// const kafka = require('kafka-node')
 
 app.use(express.json());
 app.use(cors());
 
 const db = require("./models");
+
+// const client = new kafka.KafkaClient({kafkahost: process.env.KAFKA_BOOTSTRAP_SERVERS})
+// const consumer = new kafka.Consumer(client, [{topic: process.env.KAFKA_TOPIC}], {
+//   autoCommit: false
+// })
+
+// consumer.on('message', async (msg) => {
+//   console.log(msg);
+// })
+
+// consumer.on('error', (err) => {
+//   console.log(err)
+// })
 
 // Routers
 const userRouter = require("./routes/userRouter");
@@ -16,9 +30,12 @@ app.use("/history", historyRouter);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
-
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+try {
+  db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   });
-});
+} catch (err) {
+  console.log(err);
+}
