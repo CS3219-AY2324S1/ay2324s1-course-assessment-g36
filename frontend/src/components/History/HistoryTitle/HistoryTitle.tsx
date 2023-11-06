db6a6c43b96a2598ab4dfeacf9f27babdfa4a2d5
fannyjian <fannyj@u.nus.edu>
2023-11-06 17:04:11 +0800

    refactor out  into util function

--- frontend/src/components/History/HistoryTitle/HistoryTitle.tsx
+++ frontend/src/components/History/HistoryTitle/HistoryTitle.tsx
@@ -2,6 +2,7 @@ import styles from "./HistoryTitle.module.css"
 import { Tooltip, useDisclosure } from '@chakra-ui/react'
 import HistoryEditor from "../HistoryEditor/HistoryEditor"
 import { Attempt } from "@/interfaces"
+import { truncateTitle } from "@/utils/formats"
 
 interface IOwnProps {
   attempt: Attempt
@@ -9,19 +10,12 @@ interface IOwnProps {
 
 export default function HistoryTitle({ attempt }: IOwnProps): JSX.Element {
 
-  function truncateTitle(): string {
-    if (attempt.title.length > 35) {
-      return attempt.title.slice(0, 35) + '...'
-    }
-    return attempt.title
-  }
-
   const { isOpen, onOpen, onClose } = useDisclosure()
   
   return (
     <>
       <Tooltip hasArrow label="View attempt" placement="left" aria-label="A tooltip to view question details">
-        <span className={styles.history_title} onClick={onOpen}>{truncateTitle()}</span>
+        <span className={styles.history_title} onClick={onOpen}>{truncateTitle(attempt.title)}</span>
       </Tooltip>
       <HistoryEditor attempt={attempt} isOpen={isOpen} onClose={onClose}/>
     </>
