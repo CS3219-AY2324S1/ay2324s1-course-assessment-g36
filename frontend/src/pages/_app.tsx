7d9f5bba929b4101fed4b3120d08b18bc3c9eff8
Benjamin Tan <benjamin@dev.ofcr.se>
2023-11-08 03:09:04 +0800

    Code editor: add styling for remote cursor

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "@/utils/auth";
 
+import "@/components/CodeRoom/CodeEditor/CodeEditor.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
