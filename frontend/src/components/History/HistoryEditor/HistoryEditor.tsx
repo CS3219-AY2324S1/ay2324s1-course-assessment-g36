50d39062d86b1d233fe5c508ddabb206e9417eb9
fannyjian <fannyj@u.nus.edu>
2023-11-10 12:48:44 +0800

    fix styles and add link button

--- frontend/src/components/History/HistoryEditor/HistoryEditor.tsx
+++ frontend/src/components/History/HistoryEditor/HistoryEditor.tsx
@@ -16,6 +16,7 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useAuth } from "@/utils/auth";
+import Link from "next/link";
 
 interface IOwnProps {
   attempt: Attempt;
@@ -74,16 +75,22 @@ export default function HistoryEditor({
         <ModalHeader>{attempt.title}</ModalHeader>
         <ModalCloseButton />
         <ModalBody className={styles.body}>
-          <div className={styles.date}>{attempt.date}</div>
-          <div className={styles.bodyContainer}>
-            <div className={styles.description}>
-              {attempt.description.split("\n").map((desc, index) => (
-                <p key={index}>
-                  {desc}
-                  <br />
-                </p>
-              ))}
+            <div className={styles.bodyContainer}>
+              <div className={styles.detailsContainer}>
+                <div className={styles.date}>{attempt.date}</div>
+                  <div className={styles.description}>
+                    {attempt.description.split("\n").map((desc, index) => (
+                      <p key={index}>
+                        {desc}
+                        <br />
+                      </p>
+                    ))}
+                  </div>
+                <Link href={attempt.link}>
+                  <Button colorScheme="gray" className={styles.checkout}>Check out here</Button>
+                </Link>
             </div>
+            
             <div className={styles.inputContainer}>
               <div className={styles.selectContainer}>
                 <Select
@@ -112,7 +119,7 @@ export default function HistoryEditor({
                 onChange={(e) => handleCodeChange(e)}
               />
             </div>
-          </div>
+          </div> 
         </ModalBody>
         <ModalFooter>
           <Button colorScheme="green" onClick={() => updateAttempt()}>
