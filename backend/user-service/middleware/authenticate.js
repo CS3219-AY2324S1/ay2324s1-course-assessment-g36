6c3ea0b4b6f58b10c10cbd04f686db6c7815a149
Benjamin Tan <benjamin@dev.ofcr.se>
2023-11-14 22:51:49 +0800

    Backend/User service: `npm run format`

--- backend/user-service/middleware/authenticate.js
+++ backend/user-service/middleware/authenticate.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const getUserFromToken = (req) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    const user = jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRET);
-    return user;
-}
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  const user = jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRET);
+  return user;
+};
 
 const authenticateHistory = (req, res, next) => {
   try {
@@ -17,41 +17,41 @@ const authenticateHistory = (req, res, next) => {
 };
 
 const authenticateAdmin = (req, res, next) => {
-    if (["/login", "/register"].includes(req.path)) {
-      next();
-      return;
-    }
-  
-    try {
-        const user = getUserFromToken(req);
-        if (!user.isAdmin) {
-            return res.sendStatus(401);
-        }
-    } catch {
+  if (["/login", "/register"].includes(req.path)) {
+    next();
+    return;
+  }
+
+  try {
+    const user = getUserFromToken(req);
+    if (!user.isAdmin) {
       return res.sendStatus(401);
     }
-    next();
-  };
-  
-const authenticateProfile = (req, res, next, value) => {
-    try {
-        const user = getUserFromToken(req);
-        const requestType = req.method;
+  } catch {
+    return res.sendStatus(401);
+  }
+  next();
+};
 
-        if (
-        `${user.userId}` !== value &&
-        !(user.isAdmin && requestType !== "PUT")
-        ) {
-        return res.sendStatus(401);
-        }
-    } catch {
-        return res.sendStatus(401);
+const authenticateProfile = (req, res, next, value) => {
+  try {
+    const user = getUserFromToken(req);
+    const requestType = req.method;
+
+    if (
+      `${user.userId}` !== value &&
+      !(user.isAdmin && requestType !== "PUT")
+    ) {
+      return res.sendStatus(401);
     }
-    next();
+  } catch {
+    return res.sendStatus(401);
+  }
+  next();
 };
 
 module.exports = {
-    authenticateHistory,
-    authenticateAdmin,
-    authenticateProfile
-}
+  authenticateHistory,
+  authenticateAdmin,
+  authenticateProfile,
+};
