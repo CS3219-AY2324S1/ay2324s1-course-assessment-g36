41d1c97d3c2707407676a230a2baab342a62a44f
Benjamin Tan <benjamin@dev.ofcr.se>
2023-11-14 20:43:15 +0800

    `npm run format`

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,11 @@
-const QUESTIONS_URI = process.env.QUESTION_SERVICE_URI || process.env.NEXT_PUBLIC_QUESTION_SERVICE_URI || "http://localhost:3001";
-const USERS_URI = process.env.NEXT_PUBLIC_USER_SERVICE_URI || "http://localhost:8000" ;
-export const MATCHING_URI = process.env.NEXT_PUBLIC_MATCHING_SERVICE_URI || "ws://localhost:3002/" ;
+const QUESTIONS_URI =
+  process.env.QUESTION_SERVICE_URI ||
+  process.env.NEXT_PUBLIC_QUESTION_SERVICE_URI ||
+  "http://localhost:3001";
+const USERS_URI =
+  process.env.NEXT_PUBLIC_USER_SERVICE_URI || "http://localhost:8000";
+export const MATCHING_URI =
+  process.env.NEXT_PUBLIC_MATCHING_SERVICE_URI || "ws://localhost:3002/";
 
 export const QUESTIONS_API = `${QUESTIONS_URI}/questions`;
 
